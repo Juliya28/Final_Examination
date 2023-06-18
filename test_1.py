10719f6cab1a0f6396a451c18edb3a887661c8b3
Juliya <yuliya_2808@mail.ru>
2023-06-18 03:17:03 +0300

    Final exam

--- test_1.py
+++ test_1.py
@@ -17,15 +17,3 @@ def test_step1(browser):
     testpage.get_about_page_text()
     testpage.check_about_page_title_font_size()
 
-   
-# def test_page_loading(browser, about_page):
-#     page = Operations(browser)
-#     page.click_about_button()
-#     assert page.get_about_page_text() == about_page, \
-#         'Страница About не загружается'
-
-
-# def test_font_size(browser, font_size):
-#     page = Operations(browser)
-#     assert page.check_about_page_title_font_size(font_size), \
-#         f'Шрифт заголвка About Page не равен {font_size}'
